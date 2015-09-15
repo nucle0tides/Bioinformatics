d4e5b77fea6c06ac3cbabb2e6331b99aea16fd51
Gabby <ortmangm@iastate.edu>
2015-09-15 11:52:41 -0500

    work on more of project

--- Project 2/project_2_job4.pl
+++ Project 2/project_2_job4.pl
@@ -3,26 +3,35 @@
 use strict;
 use warnings;
 
+my $line_width = 70;
+# this subroutine prints $seq out, broken into $line_width pieces
+sub print_seq {
+ for (my $i=0; $i<length($seq); $i+=$line_width) {
+ print substr($seq, $i, $line_width), "\n"; # output FASTA lines
+ }
+}
+
 #initialize count and seq as 0 and an empty string 
 my ($count, $seq) = ( 0, "");
 while (<>) {
   if (/^>/) {
     if (length($seq)) {
-      print "$seq\n";
+      &print_seq
     }
     print;
     #reset sequence sequence 
     $seq = "";
     #update count
     $count++;
-    
   } 
 
   else {
     #remove trailing newline character
     chomp;
     $seq = $seq . $_;
-    print"$seq\n"
+    &print_seq
   }
 }
 print STDERR "there are $count input sequences\n";
+
+
