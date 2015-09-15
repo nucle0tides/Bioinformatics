d2906f89dc9dc614ba68cb92dd40600d9527b835
Gabby <ortmangm@iastate.edu>
2015-09-15 11:31:04 -0500

    add project 2

---
+++ Project 2/project_2_job1.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my ($count, $seq) = ( 0, "");
+while (<>) {
+  if (/^>/) {
+    if (length($seq)) {
+      print "$seq\n";
+    }
+    print;
+    $count++;
+    $seq = "";
+  } else {
+    #chomp;
+    $seq = $seq . $_;
+  }
+}
+print STDERR "there are $count input sequences\n";
